aa4e00e7d14c317c67bcead441c9826e66a61622
JamesDodd1 <james.dodd700@gmail.com>
2020-10-06 23:30:03 +0100

    Created dictionary files

---
+++ PhoneTranslate/PhoneTranslate/Dictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneTranslate
+{
+    class Dictionary
+    {
+        private string file;
+
+
+        /// <summary> Initalises a new instance of Dictionary </summary>
+        public Dictionary() 
+        {
+            this.file = @".\..\..\files\Dictionary.txt";
+        }
+
+
+        public void Read()
+        {
+            string normal, slang, swear;
+
+            using (StreamReader reader = new StreamReader(file, Encoding.UTF8))
+            {
+                List<string> list = new List<string>();
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    slang = line;
+                    normal = reader.ReadLine();
+                    swear = reader.ReadLine();
+                    reader.ReadLine(); // Spacing line
+                }
+
+                Console.WriteLine("Done");
+            }
+        }
+    }
+}
